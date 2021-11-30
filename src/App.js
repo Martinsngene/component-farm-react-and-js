086262d3207456ba152da3833a1392a0a2c13f4b
Martins Ngene <martinsngene9@gmail.com>
2021-11-30 07:27:23 +0100

    [update] on project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
 import React from "react";
-import { Container } from "react-bootstrap";
 
 function App() {
-  return (
-    <div className="App">
-      <Container>
-        <h1>Hello World</h1>
-      </Container>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
